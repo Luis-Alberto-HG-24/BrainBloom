bcc8dd5a9fe8492f534bd00fc78f607c7acd6276
Luis-Alberto-HG-24 <luisalbertohge.e@gmail.com>
2024-03-09 17:06:59 -0600

    Agregado del README

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts/main')
 @section('contenido')
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi recusandae sapiente explicabo, illum nemo ratione. Aspernatur, unde ea laudantium veniam quasi quo modi mollitia in, debitis tempora facilis fugiat totam.
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>title</title>
 </head>
 <body>
+    @include('sweetalert::alert')
+
     @yield('contenido')
+    <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
